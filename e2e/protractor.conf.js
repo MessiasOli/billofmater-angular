58424aa9b08dc98a9e3784eb6b075c90418d1833
Messias Oliveira <50084447+MessiasOli@users.noreply.github.com>
2021-06-26 10:57:10 -0300

    Ajuste de package.json

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -17,11 +17,11 @@ exports.config = {
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
-  baseUrl: 'http://localhost:8080/',
+  baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: 60000,
     print: function() {}
   },
   onPrepare() {
